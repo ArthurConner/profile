c1ea0152fe53c746a8fc7b29d79063285fc382b1
Arthur <aconner@earthlink.net>
2016-09-21 12:24:37 -0700

    building source

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,26 @@ router.get('/about.html', function(req, res, next) {
 });
 
 
+router.get('/history.html', function(req, res, next) {
+  res.render('history', { title: 'About' });
+});
+
+
+//"http://meeting.support:3001/podcast?name=Klute&url=http://www.stevenbenedict.ie/wp-content/uploads/Klute.mp3&subtitle=Steven%20Benedict&imageList="
+//http://localhost:3000/podcast?name=Klute&url=http://www.stevenbenedict.ie/wp-content/uploads/Klute.mp3&subtitle=Steven%20Benedict&imageList=
+router.get('/podcast', function(req, res, next) {
+  //res.render('history', { title: 'About' });
+
+var route =  req.param("url")
+ 
+ console.log('url:', route)
+  if (route) {
+ console.log('url param was detected: ', route)
+  res.redirect(route);
+  } else {
+  res.render('index', { title: route });
+   console.log('url param was missing: ', route)
+  }
+});
 
 module.exports = router;
